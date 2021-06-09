5236b877d408fd19e3137deb14404ba4203bb7d7
bbaatar <bbaatar@e3r9p15.42.fr>
2021-06-09 16:26:25 +0200

    changed gnl

--- projects/get_next_line/get_next_line.c
+++ projects/get_next_line/get_next_line.c
@@ -31,7 +31,10 @@ int get_next_char(int fd, char *ch)
 	{
 		fd_res = read(fd, buff, BUFFER_SIZE);
 		if (fd_res <= 0)
+		{
+			// printf("fd_res %d\n", fd_res);
 			return (fd_res);
+		}
 		i = 0;
 		*ch = buff[i++];
 		return (1);
@@ -42,52 +45,61 @@ int	get_next_line(int fd, char **line)
 {
 	int		i;
 	char	*res;
+	int 	tmp;
 
 	if (fd < 0 || !line || BUFFER_SIZE <= 0)
 		return (-1);
 	res = (char *)malloc(BUFFER_SIZE + 1);
 	if (!res)
-		return (0);
+		return (-1);
 	i = 0;
 	while (1)
 	{
-		if (get_next_char(fd, &res[i]) < 0)
+		tmp = get_next_char(fd, &res[i]);
+		if (tmp < 0)
 		{
 			free(res);
 			return (-1);
 		}
-		if (!res[i])
+		if (!tmp)
+		{
+			res[i] = 0;
+			// printf("res[i], i : %d\n", i);
 			return (0);
+		}
+		// if (!res[i])
+		// 	return (0);
 		if (res[i] == '\n')
 			break ;
 		i++;
 	}
 	res[i] = '\0';
 	*line = &res[0];
+	free(res);
 	return (1);
 }
 
-// int   main()
-// {
-//   char  *line;
-//   int   fd3;
+int   main()
+{
+  char  *line;
+  int   fd;
 
-//   fd3 = open("file2", O_RDONLY);
+  fd = open("empty_file", O_RDONLY);
 
-//   get_next_line(fd3, &line);
-//   printf("current buff is : %s\n", line);
+  get_next_line(fd, &line);
+  printf("current buff is : %s\n", line);
   
-//   get_next_line(fd3, &line);
-//   printf("current buff is : %s\n", line);
+  get_next_line(fd, &line);
+  printf("current buff is : %s\n", line);
   
-//   get_next_line(fd3, &line);
-//   printf("current buff is : %s\n", line);
+  get_next_line(fd, &line);
+  printf("current buff is : %s\n", line);
 
-//   get_next_line(fd3, &line);
-//   printf("current buff is : %s\n", line);
+  get_next_line(fd, &line);
+  printf("current buff is : %s\n", line);
 
-//   get_next_line(fd3, &line);
-//   printf("current buff is : %s\n", line);
+  get_next_line(fd, &line);
+  printf("current buff is : %s\n", line);
 
-//   return (0);
-// }
+  return (0);
+}
