2da820126eb38aed22de52b32e90a7f0f2364f28
bbaatar <baigalma@gmail.com>
2021-09-20 16:24:46 +0200

    adding push_swap

---
+++ projects/push_swap/include/push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbaatar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 15:11:32 by bbaatar           #+#    #+#             */
+/*   Updated: 2021/09/20 15:11:34 by bbaatar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <limits.h>
+# include "../libft/libft.h"
+
+void di_putnbr_base(long *nbr, struct s_format *format, char *buff, int *i);
+
+#endif
