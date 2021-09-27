0aa4e979f7071b4588a07648b2ea8cded8821581
bbaatar <baigalma@gmail.com>
2021-09-27 10:57:27 +0200

    deleting the printf testing folder

--- projects/testing_ft_printf/test.c
+++
@@ -1,9 +0,0 @@
-#include <libc.h>
-#include "include/ft_printf.h"
-#include <limits.h>
-
-int main()
-{
-	printf("The REAL PRINTF says : %d\n", printf(" hello |%20.12u| |%20.12d| |%20.12i| |%20.12x|\n", -25, -25, -25, -25));
-	printf("YOUR ARTISINAL PRINTF says : %d\n", ft_printf(" hello |%20.12u| |%20.12d| |%20.12i| |%20.12x|\n", -25, -25, -25, -25));
-}
