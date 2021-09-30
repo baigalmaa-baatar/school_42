09a01ac8f9ff107b36e3713e9e992b31f5c7b5ce
bbaatar <baigalma@gmail.com>
2021-09-30 13:36:25 +0200

    adding ft_atoi.c

---
+++ projects/libft/with_main_libft/ft_atoi.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbaatar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/24 16:00:31 by bbaatar           #+#    #+#             */
+/*   Updated: 2021/09/30 13:27:49 by bbaatar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_skip_spaces(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+		i++;
+	return (i);
+}
+
+int	ft_atoi(const char *str)
+{
+	size_t				i;
+	unsigned long long	res;
+	int					neg;
+
+	res = 0;
+	neg = 1;
+	i = 0;
+	i = ft_skip_spaces(str);
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			neg *= (-1);
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		res = res * 10 + (str[i] - 48);
+		i++;
+	}
+	if (res > LLONG_MAX && neg == 1)
+		return (-1);
+	if (res > LLONG_MAX && neg == -1)
+		return (0);
+	return (res * neg);
+}
+
+int main(int argc, char *argv[])
+{
+	int	i1;
+	int	i2;
+
+	(void)argc;
+	i1 = atoi(argv[1]);
+	i2 = ft_atoi(argv[2]);
+
+	printf("Std function : %d\nMy function  : %d\n", i1, i2);
+
+	return (0);
+}
