09c32ff7b0dc8eb7362522c4554a06000eb67c76
stardustd <baigalma@gmail.com>
2021-12-10 23:19:00 +0100

    changed the zoom in zoom out logic

--- projects/fdf/mac/srcs/main.c
+++ projects/fdf/mac/srcs/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbaatar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 19:12:20 by bbaatar           #+#    #+#             */
+/*   Updated: 2021/12/10 19:12:21 by bbaatar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <mlx.h>
 #include <math.h>
 #include "fdf.h"
@@ -11,6 +23,61 @@ typedef struct s_data
 	int endian;
 } t_data;
 
+typedef struct s_image {
+	long long points[1000][1000];
+	int width;
+	int height;
+	void *mlx;
+	void *window;
+} t_image;
+
+double cameraX = 38;
+double cameraY = 22;
+double cameraZ = 40;
+double zFactor = 1;
+double xyFactor = 10;
+
+// https://en.wikipedia.org/wiki/Euler_angles#Tait%E2%80%93Bryan_angles
+double thetaX = 0.785398; // 45 degree
+double thetaY = 0.785398; // 45 degree
+double thetaZ = -0.785398; // -45 degree
+
+double epsilonX = 200;
+double epsilonY = -20;
+double epsilonZ = 50;
+
+void transform(double aX, double aY, double aZ, double *bX, double *bY) {
+	// https://en.wikipedia.org/wiki/3D_projection#Mathematical_formula
+	/*
+    double x = aX - cameraX;
+    double y = aY - cameraY;
+    double z = aZ - cameraZ;
+    double cX = cos(thetaX);
+    double cY = cos(thetaY);
+    double cZ = cos(thetaZ);
+    double sX = sin(thetaX);
+    double sY = sin(thetaY);
+    double sZ = sin(thetaZ);
+    double dX = cY * (sZ * y + cZ * x) - sY * z;
+    double dY = sX * (cY * z + sY * (sZ * y + cZ * x)) + cX * (cZ * y - sZ * x);
+    double dZ = cX * (cY * z + sY * (sZ * y + cZ * x)) - sX * (cZ * y - sZ * x);
+
+    *bX = epsilonZ / dZ * dX + epsilonX;
+    *bY = epsilonZ / dZ * dY + epsilonY;
+	*/
+
+	// https://en.wikipedia.org/wiki/Isometric_projection#Mathematics
+	aZ *= zFactor;
+	*bX = (aX - aZ) / sqrt(2);
+	*bY = (aX + 2 * aY + aZ) / sqrt(6);
+	*bX *= xyFactor;
+	*bY *= xyFactor;
+	*bX += 100;
+	*bY += 100;
+
+	// printf("%0.3lf %0.3lf\n", *bX, *bY);
+}
+
 void my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char *dst;
@@ -19,7 +86,7 @@ void my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-void plot_line_low(int x0, int y0, int x1, int y1, t_data img)
+void plot_line_low(int x0, int y0, int x1, int y1, t_data *img)
 {
 	int dx = x1 - x0;
 	int dy = y1 - y0;
@@ -38,7 +105,7 @@ void plot_line_low(int x0, int y0, int x1, int y1, t_data img)
 	x = x0;
 	while (x < x1)
 	{
-		my_mlx_pixel_put(&img, x, y, 0x00f4c0ff);
+		my_mlx_pixel_put(img, x, y, 0x00f4c0ff);
 		if (d > 0)
 		{
 			y = y + yi;
@@ -50,7 +117,7 @@ void plot_line_low(int x0, int y0, int x1, int y1, t_data img)
 	}
 }
 
-void plot_line_high(int x0, int y0, int x1, int y1, t_data img)
+void plot_line_high(int x0, int y0, int x1, int y1, t_data *img)
 {
 	int dx = x1 - x0;
 	int dy = y1 - y0;
@@ -69,7 +136,7 @@ void plot_line_high(int x0, int y0, int x1, int y1, t_data img)
 	x = x0;
 	while (y < y1)
 	{
-		my_mlx_pixel_put(&img, x, y, 0x00f4c0ff);
+		my_mlx_pixel_put(img, x, y, 0x00f4c0ff);
 		if (d > 0)
 		{
 			x = x + xi;
@@ -80,7 +147,7 @@ void plot_line_high(int x0, int y0, int x1, int y1, t_data img)
 		y++;
 	}
 }
-void	plot_line(int x0, int y0, int x1, int y1, t_data img)
+void	plot_line(int x0, int y0, int x1, int y1, t_data *img)
 {
 	if (abs(y1 - y0) < abs(x1 - x0))
 	{
@@ -98,131 +165,155 @@ void	plot_line(int x0, int y0, int x1, int y1, t_data img)
 	}
 }
 
-double cameraX = -10;
-double cameraY = 0;
-double cameraZ = -100;
-double thetaX = 2.0944;
-double thetaY = 2.0944;
-double thetaZ = 2.0944;
-double epsilonX = 1200;
-double epsilonY = 800;
-double epsilonZ = 400;
+int	render_next_frame(t_image *data) {
+	int x, y;
+	double x1, y1, x2, y2;
+	t_data		img;
 
-void translate(double aX, double aY, double aZ, double *bX, double *bY) {
-    double x = aX - cameraX;
-    double y = aY - cameraY;
-    double z = aZ - cameraZ;
-    double cX = cos(thetaX);
-    double cY = cos(thetaY);
-    double cZ = cos(thetaZ);
-    double sX = sin(thetaX);
-    double sY = sin(thetaY);
-    double sZ = sin(thetaZ);
-    double dX = cY * (sZ * y + cZ * x) - sY * z;
-    double dY = sX * (cY * z + sY * (sZ * y + cZ * x)) + cX * (cZ * y - sZ * x);
-    double dZ = cX * (cY * z + sY * (sZ * y + cZ * x)) - sX * (cZ * y - sZ * x);
-    
-    *bX = epsilonZ / dZ * dX + epsilonX;
-    *bY = epsilonZ / dZ * dY + epsilonY;
+	img.img = mlx_new_image(data->mlx, 600, 600);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+
+	y = 0;
+	while (y < data->height)
+	{
+		x = 0;
+		while (x < data->width - 1)
+		{
+			transform(x, y, data->points[y][x], &x1, &y1);
+			transform(x + 1, y, data->points[y][x + 1], &x2, &y2);
+			if (0 <= x1 && x1 < 600 && 0 <= y1 && y1 < 600) {
+				if (0 <= x2 && x2 < 600 && 0 <= y2 && y2 < 600) {
+					plot_line(x1, y1, x2, y2, &img);
+				}
+			}
+			x++;
+		}
+		y++;
+	}
+
+	x = 0;
+	while (x < data->width)
+	{
+		y = 0;
+		while (y < data->height - 1)
+		{
+			transform(x, y, data->points[y][x], &x1, &y1);
+			transform(x, y + 1, data->points[y + 1][x], &x2, &y2);
+			if (0 <= x1 && x1 < 600 && 0 <= y1 && y1 < 600) {
+				if (0 <= x2 && x2 < 600 && 0 <= y2 && y2 < 600) {
+					plot_line(x1, y1, x2, y2, &img);
+				}
+			}
+			y++;
+		}
+		x++;
+	}
+
+	mlx_put_image_to_window(data->mlx, data->window, img.img, 0, 0);
+	mlx_destroy_image(data->mlx, img.img);
+
+	return 1;
+}
+
+int	key_hook(int keycode, t_image *data)
+{
+	if (keycode == 126) {
+		// UP
+		cameraX += 1;
+		zFactor += 0.1;
+	}
+	if (keycode == 125) {
+		// DOWN
+		cameraX -= 1;
+		zFactor -= 0.1;
+	}
+	if (keycode == 123) {
+		// LEFT
+		cameraY -= 1;
+		xyFactor += 0.5;
+	}
+	if (keycode == 124) {
+		// RIGHT
+		cameraY += 1;
+		xyFactor -= 0.5;
+	}
+	if (keycode == 116) {
+		// PAGE UP
+		cameraZ += 1;
+	}
+	if (keycode == 121) {
+		// PAGE DOWN
+		cameraZ -= 1;
+	}
+
+	double centerX = data->width / 2;
+	double centerY = data->height / 2;
+	double centerZ = 5;
+	double distance = sqrt(pow(cameraX - centerX, 2) + pow(cameraY - centerY, 2) + pow(cameraZ, 2));
+
+	// https://www.quora.com/How-do-I-find-the-angle-of-a-3D-vector-i-e-A-2i-3j-5k
+	thetaX = asin((cameraX - centerX) / distance);
+	thetaY = asin((cameraY - centerY) / distance);
+	thetaZ = asin((cameraZ - centerZ) / distance);
+
+	if (keycode == 13) {
+		// W
+		epsilonX += 1;
+	}
+	if (keycode == 1) {
+		// S
+		epsilonX -= 1;
+	}
+	if (keycode == 0) {
+		// A
+		epsilonY -= 1;
+	}
+	if (keycode == 2) {
+		// D
+		epsilonY += 1;
+	}
+	if (keycode == 17) {
+		// T
+		epsilonZ += 1;
+	}
+	if (keycode == 5) {
+		// G
+		epsilonZ -= 1;
+	}
+
+	// printf("%d\n", keycode);
+
+	return 1;
 }
 
 int	main(int argc, char *argv[])
 {
 	char		*line;
 	int			fd;
-	int			i;
-	int			j;
-	int			row = 0;
-	int 		col = 0;
-	long long	nbrs[1000][1000];
-	int			x0;
-	int			x1;
-	int			y0;
-	int			y1;
-	void		*mlx;
-	void		*mlx_win;
-	t_data		img;
+	int			width = 0;
+	int 		height = 0;
+
+	t_image		input;
 
 	if (argc <= 1)
 		return (0);
 	fd = open(argv[1], O_RDONLY);
-	col = 0;
+	height = 0;
 	while (get_next_line(fd, &line) == 1)
 	{
 		// printf("%s\n", line);
-		row = get_numbers(line, nbrs[col]);
-		col++;
+		width = get_numbers(line, input.points[height]);
+		height++;
 		free(line);
 	}
-	printf("row is : %d\n", row);
-	printf("col is : %d\n", col);
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 600, 600, "FDF");
-	img.img = mlx_new_image(mlx, 600, 600);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	x0 = 0;
-	y0 = 0;
-	x1 = 20;
-	y1 = 20;
-	//row = x1 * row;
-	//col = y1 * col;
-
-	i = 0;
-	while (i < row)
-	{
-		j = 0;
-		while (j < col - 1)
-		{	
-			double x1, y1, x2, y2;
-			translate(j, i, nbrs[j][i], &x1, &y1);
-			translate(j + 1, i, nbrs[j + 1][i], &x2, &y2);
-			printf("%0.3lf %0.3lf -> %0.3lf %0.3lf\n", x1, y1, x2, y2);
-			if (0 <= x1 && x1 < 600 && 0 <= y1 && y1 < 600) {
-				if (0 <= x2 && x2 < 600 && 0 <= y2 && y2 < 600) {
-					plot_line(x1, y1, x2, y2, img);
-				}
-			}
-			j++;
-		}
-		i++;
-	}
-
-	j = 0;
-	while (j < col)
-	{
-		i = 0;
-		while (i < row - 1)
-		{	
-			double x1, y1, x2, y2;
-			translate(j, i, nbrs[j][i], &x1, &y1);
-			translate(j, i + 1, nbrs[j][i + 1], &x2, &y2);
-			printf("%0.3lf %0.3lf -> %0.3lf %0.3lf\n", x1, y1, x2, y2);
-			if (0 <= x1 && x1 < 600 && 0 <= y1 && y1 < 600) {
-				if (0 <= x2 && x2 < 600 && 0 <= y2 && y2 < 600) {
-					plot_line(x1, y1, x2, y2, img);
-				}
-			}
-			i++;
-		}
-		j++;
-	}
-	// while (x0 < row)
-	// {
-	// 	y0 = 0;
-	// 	y1 = 20;
-	// 	while (y0 < col)
-	// 	{
-	// 		plot_line(x0, y0, x1, y0, img);
-	// 		plot_line(x0, y0, x0, y1, img);
-	// 		plot_line(x0, y1, x1, y1, img);
-	// 		plot_line(x1, y0, x1, y1, img);
-	// 		y0 += 20;
-	// 		y1 += 20;
-	// 	}
-	// 	x0 += 20;
-	// 	x1 += 20;
-	// }
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	// printf("width is : %d\n", width);
+	// printf("heigth is : %d\n", height);
+	input.mlx = mlx_init();
+	input.window = mlx_new_window(input.mlx, 600, 600, "FDF");
+	input.height = height;
+	input.width = width;
+	mlx_loop_hook(input.mlx, render_next_frame, &input);
+	mlx_key_hook(input.window, key_hook, &input);
+	mlx_loop(input.mlx);
 	return (0);
 }
