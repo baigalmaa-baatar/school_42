708cfe8b806861a1fdf7482e9b11f09bdac4aeee
Baigalmaa BAATAR <bbaatar@42paris.fr>
2022-01-05 23:00:54 +0100

    a

--- projects/philo_one/srcs/main.c
+++ projects/philo_one/srcs/main.c
@@ -5,22 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bbaatar <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/28 20:57:37 by bbaatar           #+#    #+#             */
-/*   Updated: 2021/12/28 20:57:38 by bbaatar          ###   ########.fr       */
+/*   Created: 2022/01/05 22:59:00 by bbaatar           #+#    #+#             */
+/*   Updated: 2022/01/05 22:59:43 by bbaatar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-int str_err(char *str, int ret)
+int	str_err(char *str, int ret)
 {
 	write(1, str, ft_strlen(str));
 	return (ret);
 }
 
-int	init(t_inputVal *inputVal, unsigned long long ltaArr[], pthread_mutex_t forks[], pthread_mutex_t *message)
+int	init(t_inputVal *inputVal, unsigned long long ltaArr[],
+		pthread_mutex_t forks[], pthread_mutex_t *message)
 {
-	unsigned int i;
+	unsigned int	i;
 
 	i = 0;
 	while (i < inputVal->philoNbr)
@@ -33,13 +34,13 @@ int	init(t_inputVal *inputVal, unsigned long long ltaArr[], pthread_mutex_t fork
 	return (1);
 }
 
-int display_stat(t_philo *philos, char *str, unsigned long long timeToDo)
+int	display_stat(t_philo *philos, char *str, unsigned long long timeToDo)
 {
 	if (!*philos->running)
 		return (0);
 	pthread_mutex_lock(philos->message);
 	ft_putnbr_fd(get_time(), 1);
-	write(1," ", 1);
+	write (1," ", 1);
 	ft_putnbr_fd(philos->pId + 1, 1);
 	write(1, str, ft_strlen(str));
 	pthread_mutex_unlock(philos->message);
@@ -48,23 +49,24 @@ int display_stat(t_philo *philos, char *str, unsigned long long timeToDo)
 	return (1);
 }
 
-int	checkPhiloDied(bool running, pthread_mutex_t *leftFork, pthread_mutex_t *rightFork)
+int	checkPhiloDied(bool running, pthread_mutex_t *left_fork, pthread_mutex_t *right_fork)
 {
 	if (!running)
-		{
-			pthread_mutex_unlock(leftFork);
-			pthread_mutex_unlock(rightFork);
-			return (0);
-		}
+	{
+		pthread_mutex_unlock(left_fork);
+		pthread_mutex_unlock(right_fork);
+		return (0);
+	}
 	return (1);
 }
 
-int	checkAteEnough(int	*ateCntr, int mustEatNbr, pthread_mutex_t *leftFork, pthread_mutex_t *rightFork)
+int	checkAteEnough(int *ateCntr, int mustEatNbr, pthread_mutex_t *left_fork, 
+		pthread_mutex_t *right_fork)
 {
 	if (*ateCntr == mustEatNbr)
 	{
-		pthread_mutex_unlock(rightFork);
-		pthread_mutex_unlock(leftFork);
+		pthread_mutex_unlock(right_fork);
+		pthread_mutex_unlock(left_fork);
 		return (0);
 	}
 	else
@@ -72,57 +74,59 @@ int	checkAteEnough(int	*ateCntr, int mustEatNbr, pthread_mutex_t *leftFork, pthr
 	return (1);
 }
 
-void takeForks(t_philo *philos, pthread_mutex_t *left, pthread_mutex_t *right)
+void	takeForks(t_philo *philos, pthread_mutex_t *left, pthread_mutex_t *right)
 {
 	pthread_mutex_lock(left);
 	pthread_mutex_lock(right);
 	display_stat(philos, " has taken a fork\n", 0);
 }
 
-void releaseForks(pthread_mutex_t *left, pthread_mutex_t *right)
+void	releaseForks(pthread_mutex_t *left, pthread_mutex_t *right)
 {
 	pthread_mutex_unlock(left);
 	pthread_mutex_unlock(right);
 }
 
-void eat(t_philo *philos)
+void	eat(t_philo *philos)
 {
 	philos->ltaArr[philos->pId] = get_time();
 	display_stat(philos, " is eating\n", philos->inputVal.timeToEat);
 }
 
-void    *routine(void *arg)
+void	*routine(void *arg)
 {
-	int ateCntr;
-	pthread_mutex_t *leftFork;
-	pthread_mutex_t *rightFork;
+	int	ateCntr;
+
+	pthread_mutex_t *left_fork;
+	pthread_mutex_t *right_fork;
 	t_philo *philos;
 
 	philos = (t_philo *)arg;
 	ateCntr = 0;
-	leftFork = &philos->forks[philos->pId];
-	rightFork = &philos->forks[(philos->pId + 1) % philos->inputVal.philoNbr];
+	left_fork = &philos->forks[philos->pId];
+	right_fork = &philos->forks[(philos->pId + 1) % philos->inputVal.philoNbr];
 	usleep((philos->pId % 2) * 15000);
 	while (1)
 	{
 		if (!(*philos->running))
 			break;
-		takeForks(philos, leftFork, rightFork);
-		//if (!checkPhiloDied(*(philos->running), leftFork, rightFork))
+		takeForks(philos, left_fork, right_fork);
+		//if (!checkPhiloDied(*(philos->running), left_fork, right_fork))
 		//	break;
-		if (!checkAteEnough(&ateCntr, (int)philos->inputVal.mustEatNbr, leftFork, rightFork))
+		if (!checkAteEnough(&ateCntr, (int)philos->inputVal.mustEatNbr, left_fork, right_fork))
 			break;
 		eat(philos);
-		releaseForks(leftFork, rightFork);
+		releaseForks(left_fork, right_fork);
 		display_stat(philos, " is sleeping\n", philos->inputVal.timeToSleep);
 		display_stat(philos, " is thinking\n", 0);
 	}
 	return (NULL);
 }
 
-int small_thread(t_philo *philos, unsigned long long deltaLTA)
+int	small_thread(t_philo *philos, unsigned long long deltaLTA)
 {
-	unsigned int i;
+	unsigned int	i;
+	
 	i = 0;
 	while(i < philos->inputVal.philoNbr)
 	{
@@ -181,7 +185,7 @@ pthread_t	create_sub_thread(t_inputVal inputVal, unsigned long long ltaArr[], bo
 	deathStr.message = message;
 	deathStr.inputVal = inputVal;
 	if (pthread_create(&deathThread, NULL, &detectDeath, &deathStr) != 0)
-		return (str_err(ERR_CRT, 3));
+		str_err(ERR_CRT, 3);
 	return (deathThread);
 }
 
@@ -207,7 +211,7 @@ int	createThrds(t_inputVal inputVal, unsigned long long ltaArr[], pthread_mutex_
 			return(str_err(ERR_CRT, 3));
 		i++;
 	}
-	deathThread = create_sub_thread(inputVal, ltaArr, forks, message);
+	deathThread = create_sub_thread(inputVal, ltaArr, running, message);
 	if (joinThr(inputVal, p_th, &deathThread))
 		return (str_err(ERR_JOIN, 4));
 	return (0);
@@ -233,7 +237,7 @@ int main(int argc, char *argv[])
 	unsigned long long ltaArr[2000];
 	pthread_mutex_t forks[2000];
 	pthread_mutex_t message;
-	
+
 	if (!(get_args(argc, argv, &inputVal)))
 		return (str_err(ERR_ARG, 1));
 	if (!(init(&inputVal, ltaArr, forks, &message)))
